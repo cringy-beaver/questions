028e16119c979fa824828475d6531b34eef59e94
perceptr <st.arms.all@gmail.com>
2023-05-08 00:23:02 +0500

    Small improvements

--- db_client/ch_client.py
+++ db_client/ch_client.py
@@ -39,7 +39,8 @@ class DBClient:
         self.Question = Question
 
     async def create_table(self):
-        query = f"CREATE TABLE IF NOT EXISTS {self.Question.__tablename__} " \
+        query: str | None = f"CREATE TABLE IF NOT EXISTS " \
+                            f"{self.Question.__tablename__} " \
                 f"(id Int32, number Int32, title String, body String, " \
                 f"created_at DateTime, link_to_s3_question_pic String) " \
                 f"ENGINE = Memory"
@@ -49,7 +50,7 @@ class DBClient:
         return response.text
 
     async def insert_question(self, question):
-        query = f"INSERT INTO {self.Question.__tablename__} " \
+        query: str | None = f"INSERT INTO {self.Question.__tablename__} " \
                 f"(id, number, title, body, " \
                 f"created_at, link_to_s3_question_pic) " \
                 f"VALUES ({question['id']}, {question['number']}, " \
@@ -62,7 +63,7 @@ class DBClient:
         return response.text
 
     async def get_questions_filter_by_title(self, title_filter=None):
-        query = f"SELECT * FROM {self.Question.__tablename__}"
+        query: str | None = f"SELECT * FROM {self.Question.__tablename__}"
         if title_filter:
             query += f" WHERE title = '{title_filter}'"
         async with httpx.AsyncClient() as client:
@@ -72,7 +73,7 @@ class DBClient:
         return dumpt(response.text) if response.text else []
 
     async def get_questions_filter_by_date(self, date_filter=None):
-        query = f"SELECT * FROM {self.Question.__tablename__}"
+        query: str | None = f"SELECT * FROM {self.Question.__tablename__}"
         if date_filter:
             query += f" WHERE created_at = '{date_filter}'"
         async with httpx.AsyncClient() as client:
@@ -82,7 +83,7 @@ class DBClient:
         return dumpt(response.text) if response.text else []
 
     async def get_questions_filter_by_number(self, number_filter=None):
-        query = f"SELECT * FROM {self.Question.__tablename__}"
+        query: str | None = f"SELECT * FROM {self.Question.__tablename__}"
         if number_filter:
             query += f" WHERE number = '{number_filter}'"
         async with httpx.AsyncClient() as client:
@@ -92,7 +93,7 @@ class DBClient:
         return dumpt(response.text) if response.text else []
 
     async def get_questions_filter_by_id(self, id_filter=None) -> list[dict]:
-        query = f"SELECT * FROM {self.Question.__tablename__}"
+        query: str | None = f"SELECT * FROM {self.Question.__tablename__}"
         if id_filter:
             query += f" WHERE id = '{id_filter}'"
         async with httpx.AsyncClient() as client:
@@ -103,7 +104,7 @@ class DBClient:
 
     async def get_questions_filter_by_body(self, body_filter=None) \
             -> list[dict]:
-        query = f"SELECT * FROM {self.Question.__tablename__}"
+        query: str | None = f"SELECT * FROM {self.Question.__tablename__}"
         if body_filter:
             query += f" WHERE body = '{body_filter}'"
         async with httpx.AsyncClient() as client:
@@ -113,7 +114,7 @@ class DBClient:
         return dumpt(response.text) if response.text else []
 
     async def get_all_questions(self):
-        query = f"SELECT * FROM {self.Question.__tablename__}"
+        query: str | None = f"SELECT * FROM {self.Question.__tablename__}"
         async with httpx.AsyncClient() as client:
             response = await client.post(self.__uri, data=query,
                                          auth=self.__auth)
@@ -121,7 +122,8 @@ class DBClient:
         return dumpt(response.text) if response.text else []
 
     async def update_question(self, question_id, question):
-        query = f"ALTER TABLE {self.Question.__tablename__} UPDATE " \
+        query: str | None = f"ALTER TABLE {self.Question.__tablename__} " \
+                            f"UPDATE " \
                 f"number = {question['number']}, " \
                 f"title = '{question['title']}', " \
                 f"body = '{question['body']}', " \
@@ -136,7 +138,7 @@ class DBClient:
         return dumpt(response.text) if response.text else response.status_code
 
     async def delete_question(self, question_id):
-        query = f"ALTER TABLE {self.Question.__tablename__} " \
+        query: str | None = f"ALTER TABLE {self.Question.__tablename__} " \
                 f"DELETE WHERE id = {question_id}"
         async with httpx.AsyncClient() as client:
             response = await client.post(self.__uri, data=query,
